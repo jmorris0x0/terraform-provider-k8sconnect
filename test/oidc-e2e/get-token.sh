931ea2269f952843e15e0785eb64c8cfb3224d55
Jonathan Morris <jmorris0x0@gmail.com>
2025-05-11 20:16:24 -0700

    More

--- test/oidc-e2e/get-token.sh
+++ test/oidc-e2e/get-token.sh
@@ -20,8 +20,17 @@ RESPONSE="$(curl -s --insecure -X POST "${DEX_URL}/token" \
   -d client_secret="$CLIENT_SECRET" \
   -d scope='openid email profile')"
 
-echo "RESPONSE: $RESPONSE"
-
+#for field in access_token id_token; do
+#  TOKEN=$(echo "$RESPONSE" | jq -r --arg f "$field" '.[$f] // empty')
+#  if [ -n "$TOKEN" ]; then
+#    echo
+#    echo "=== decoded $field header ==="
+#    echo "$TOKEN" | cut -d. -f1 | base64 --decode | jq .
+#    echo "=== decoded $field payload ==="
+#    echo "$TOKEN" | cut -d. -f2 | base64 --decode | jq .
+#  fi
+#done
+#
 TOKEN="$(echo "$RESPONSE" | jq -r '.access_token // empty' | tr -d '\n')"
 
 if [[ -z $TOKEN || $TOKEN == "null" ]]; then
@@ -29,7 +38,7 @@ if [[ -z $TOKEN || $TOKEN == "null" ]]; then
   exit 1
 fi
 
-echo "TOKEN_FOR_KUBECTL = '${TOKEN}'" >>/tmp/kubectl-exec.log
+# echo "TOKEN_FOR_KUBECTL = '${TOKEN}'" >>/tmp/kubectl-exec.log
 
 EXPIRY=$(date -u -d '+5 minutes' +'%Y-%m-%dT%H:%M:%SZ' 2>/dev/null || date -u -v +5M +'%Y-%m-%dT%H:%M:%SZ')
 
