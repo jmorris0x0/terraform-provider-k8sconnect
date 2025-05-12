b72a6cacee54822e23e2a5d014aa3b9220e3a70c
Jonathan Morris <jmorris0x0@gmail.com>
2025-05-11 20:29:19 -0700

    More

--- oidc-test.sh
+++ oidc-test.sh
@@ -9,7 +9,7 @@ kubectl get namespaces \
   --token="$ACCESS_TOKEN" \
   --insecure-skip-tls-verify
 
-# 2) Direct curl to the Kubernetes API
-#curl -k \
-#  -H "Authorization: Bearer $ACCESS_TOKEN" \
-#  https://127.0.0.1:6443/api/v1/namespaces
+kubectl auth can-i --list \
+  --server="$CLUSTER_ENDPOINT" \
+  --token="$ACCESS_TOKEN" \
+  --insecure-skip-tls-verify
