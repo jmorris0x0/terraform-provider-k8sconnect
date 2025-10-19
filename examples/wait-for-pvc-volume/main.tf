0a56ddc192ce72e209eeb3084d99293485393070
Jonathan Morris <jmorris0x0@gmail.com>
2025-10-18 22:44:26 -0700

    Fix codecov

--- examples/wait-for-pvc-volume/main.tf
+++ examples/wait-for-pvc-volume/main.tf
@@ -13,28 +13,8 @@ resource "k8sconnect_object" "namespace" {
   cluster_connection = var.cluster_connection
 }
 
-# Create PersistentVolume
-resource "k8sconnect_object" "pv" {
-  yaml_body = <<-YAML
-    apiVersion: v1
-    kind: PersistentVolume
-    metadata:
-      name: data-pv
-    spec:
-      capacity:
-        storage: 5Gi
-      accessModes:
-        - ReadWriteOnce
-      persistentVolumeReclaimPolicy: Delete
-      storageClassName: manual
-      hostPath:
-        path: /tmp/data
-  YAML
-
-  cluster_connection = var.cluster_connection
-}
-
-# Create PVC
+# Create PVC using k3d's local-path storage class (dynamic provisioning)
+# No need to manually create PV - the provisioner creates it automatically
 resource "k8sconnect_object" "pvc" {
   yaml_body = <<-YAML
     apiVersion: v1
@@ -45,14 +25,14 @@ resource "k8sconnect_object" "pvc" {
     spec:
       accessModes:
         - ReadWriteOnce
-      storageClassName: manual
+      storageClassName: local-path
       resources:
         requests:
           storage: 5Gi
   YAML
 
   cluster_connection = var.cluster_connection
-  depends_on         = [k8sconnect_object.namespace, k8sconnect_object.pv]
+  depends_on         = [k8sconnect_object.namespace]
 }
 
 # Wait for PVC to be bound before proceeding with dependent resources
